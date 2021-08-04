e17ee3110564469d2b22e1ea932901d761ef611b
Imran Syed Abdul <s.imran@sap.com>
2021-08-04 17:55:35 -0400

    final

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("helloworld1.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
